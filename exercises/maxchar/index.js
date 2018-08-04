863ac664e0e7382502c315feecd55ee70a3fd167
Tyler Walker <tyler.walker@dakno.com>
2018-08-03 20:44:49 -0400

    Got solution, is messy

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,26 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+  let map = {};
+  str.split('').map( (cur) => {
+    map[cur]
+      ? map[cur] += 1
+      : map[cur] = 1;
+  });
+
+  let keys = Object.keys(map);
+  let values = Object.values(map);
+  let max = Math.max(...values);
+  let answer = null;
+
+  keys.map( key => {
+    if (map[key] === max) {
+      answer = key
+    }
+  });
+
+  return answer;
+}
 
 module.exports = maxChar;
