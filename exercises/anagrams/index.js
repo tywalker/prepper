0262130a24543eccd55699d9bc05233d52a2eb6d
Tyler Walker <tyler.walker@dakno.com>
2018-08-04 10:48:11 -0400

    Second solution that will take more than two arguments

--- exercises/anagrams/index.js
+++ exercises/anagrams/index.js
@@ -8,13 +8,23 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {
+function first_anagrams(stringA, stringB) {
   let rawA = stringA.replace(/[^\w]/g, "");
   let rawB = stringB.replace(/[^\w]/g, "");
   let sortedA = rawA.split('').sort().join('').toLowerCase();
   let sortedB = rawB.split('').sort().join('').toLowerCase();
 
   return sortedA === sortedB;
+}
+
+function anagrams(stringA, stringB) {
+  let argArr = [...arguments];
+  let raw = argArr.map( arg => arg.replace(/[^\w]/g, '') );
+  let sorted = raw.map( str => str.split('').sort().join('').toLowerCase() );
+
+  let matches = true;
+  sorted.map( str => matches = sorted[0] === str );
+  return matches;
 }
 
 module.exports = anagrams;
