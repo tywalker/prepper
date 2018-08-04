f05668f9b8cc5c645cf023ea6158c0a4e63eaeda
Tyler Walker <tyler.walker@dakno.com>
2018-08-04 15:12:28 -0400

    Added notes.js file

---
+++ exercises/linkedlist/notes.js
@@ -0,0 +1,12 @@
+// Simple linked list
+// A linked list is a nodeObj with a reference to the next node
+
+let nodeOne = {
+  data: 123
+}
+
+let nodeTwo = {
+  data: 456
+}
+
+nodeOne.next = nodeTwo;
